7b266e80e6dfa580ca9c3c2f29e56fe0ce9a4ffe
Jeff Dalton <jeff@simpleaspossible.xyz>
2015-08-05 21:46:44 -0400

    First sample script.

---
+++ src/use sharp at work/Part4 DB Tasks/application/ReplaceLinqPad.fsx
@@ -0,0 +1,25 @@
+﻿System.IO.Directory.SetCurrentDirectory (__SOURCE_DIRECTORY__)
+
+#r @"packages\FSharp.Data.TypeProviders.0.0.1\lib\net40\FSharp.Data.TypeProviders.dll"
+#r @"System.Data.Linq.dll"
+
+
+open System
+open System.Linq
+open System.Data
+open Microsoft.FSharp.Data.TypeProviders
+open System.Text.RegularExpressions
+
+[<Literal>]
+let connectionString = "Data Source=(local)\sqlserver2014; Initial Catalog=SqlInFsharp; Integrated Security=true;"
+
+type Sql = SqlDataConnection<connectionString>
+let db = Sql.GetDataContext()
+db.DataContext.Log <- Console.Out
+
+query {
+    for c in db.Customer do
+    where (c.Email.EndsWith("gmail.com"))
+    select c
+    count
+}
